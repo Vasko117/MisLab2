fc3aa6d7cd35088c77545d30367edb7a0516ac52
Vasil <vsilt2002@gmail.com>
2024-01-29 01:47:04 +0100

    2nd

--- MyFirstProject/Navigator.js
+++ MyFirstProject/Navigator.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Addsubjects from './Addsubjects';
+import Addclothes from './Addclothes';
 
 const Stack = createStackNavigator();
 
 const Navigator = () => {
-  return (
-    <Stack.Navigator screenOptions={{
-        headerStyle: {
-          backgroundColor: '#009688', // Background color of the header
-        },
-        headerTitleStyle: {
-          fontWeight: 'bold',
-          color: '#fff', // Text color of the header title
-        },
-        headerTintColor: '#fff', // Color of the back button and header buttons
-      }}>
-      <Stack.Screen
-        name="Addsubjects"
-        component={Addsubjects}
-        options={({ route }) => ({
-          title: `213178`,
-        })}
-      />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: '#009688', // Background color of the header
+				},
+				headerTitleStyle: {
+					fontWeight: 'bold',
+					color: '#fff', // Text color of the header title
+				},
+				headerTintColor: '#fff', // Color of the back button and header buttons
+			}}
+		>
+			<Stack.Screen
+				name="Addclothes"
+				component={Addclothes}
+				options={({ route }) => ({
+					title: `213178`,
+				})}
+			/>
+		</Stack.Navigator>
+	);
 };
 
 export default Navigator;
